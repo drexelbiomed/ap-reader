4e5dc6833d2fb2aac8ea794554f6508894d1fb83
Carlos Diaz <cdiaz86@gmail.com>
2020-07-20 11:08:43 -0400

    Creates unique object from all AP data entries.

--- readData.js
+++ readData.js
@@ -6,3 +6,38 @@ function dataToJSON(excelSheet) {
 }
 
 const apDataObjects = dataToJSON(initialData);
+
+const mergedStudentObjects = {};
+
+// sample entry:
+// {
+//   Student: 'Wertz, Michael Thomas',
+//   'Univ Id': '14418087',
+//   'Coll Code 1': 'R',
+//   'Majr Code 1': 'BME',
+//   'Majr Code Conc 1': '5COP',
+//   'Degc Code 1': 'BSBE',
+//   'Test Code': 'AP37',
+//   'Test Desc': 'AP English Literature & Comp',
+//   'Test Score': '4',
+//   'AP Load Date': 44025.36252314815,
+//   'Advisor Id': '10873525',
+//   'Advisor Name': 'Bryers, Elise'
+// }
+
+apDataObjects.forEach((recordInstance, index) => {
+  const studentId = recordInstance['Univ Id'];
+  const studentAdvisor = recordInstance['Advisor Name'];
+  const ifIdFound = mergedStudentObjects.hasOwnProperty(studentId);
+  if (ifIdFound) {
+    // TODO courses will be merged here
+  } else {
+    mergedStudentObjects[studentId] = {
+      name: recordInstance.Student,
+      id: studentId,
+      advisor: studentAdvisor,
+    }
+  }
+})
+
+console.log(mergedStudentObjects);
