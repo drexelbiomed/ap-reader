bb76cc7b4edf8b68a1ef2bbc0e1dd494d8599346
Carlos Diaz <cdiaz86@gmail.com>
2020-07-23 17:11:22 -0400

    Updates console output.

--- assignCourseCredit.js
+++ assignCourseCredit.js
@@ -5,23 +5,23 @@ function apCreditAssignment(students, apTests) {
   // TODO capture final information in array of credits awarded.
   const creditsAwarded = [];
   for (let student of Object.values(students)) {
-    // if (student.id === 14347618) {
-      console.log(`******* ${student.name} advised by ${student.advisor}`);
+      console.log('********************************************************');
+      console.log(`${student.name} advised by ${student.advisor}`);
       for (let testResult of student.tests) {
         for (let apTest of apTests) {
           
           if (testResult.testcode === apTest.number) {
             // TODO Before returning values, make sure credit has not already been awarded for AP test.
             if (Number(testResult.testscore) === 5 || Number(testResult.testscore) === 4) {
-              console.log("***");
-              console.log(`*** Score Required for ${apTest.name}: ${apTest.score}, Score Earned: ${testResult.testscore}`);
-              console.log(`*** Earned credit for ${apTest.courseAwarded}: ${apTest.creditAwarded} credits`);
-              console.log("***");
+              console.log("*********");
+              console.log(`Score Required for ${apTest.name}: ${apTest.score}, Score Earned: ${testResult.testscore}`);
+              console.log(`Earned credit for ${apTest.courseAwarded}: ${apTest.creditAwarded} credits`);
+              console.log("*********");
             }
           }
         }
       }
-    // }
+    console.log('********************************************************');
   }
 
   return creditsAwarded;
