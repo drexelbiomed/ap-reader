57c4538f846fe43f5cdd6257a537f1628bc390c3
Carlos Diaz <cdiaz86@gmail.com>
2020-07-23 10:52:35 -0400

    Checks which ap exams, againsts crosswalk data, student took.

--- assignCourseCredit.js
+++ assignCourseCredit.js
@@ -1,8 +1,18 @@
 const studentApData = require('./extractApData.js');
-const ApCrosswalkData = require('./extractCrosswalkData.js');
+const apCrosswalkData = require('./extractCrosswalkData.js');
 
-Object.values(studentApData).forEach(studentEntry => {
-  console.log(studentEntry);
-})
+function apCreditAssignment(students, apTests) {
+  for (let student of Object.values(students)) {
+    if (student.id === 14347618) {
+      for (let testResult of student.tests) {
+        for (let apTest of apTests) {
+          if (apTest.number === testResult.testcode) {
+            console.log(`${student.name} took the ${apTest.number}: ${apTest.name}`);
+          }
+        }
+      }
+    }
+  }
+}
 
-console.log(ApCrosswalkData);
+apCreditAssignment(studentApData, apCrosswalkData)
