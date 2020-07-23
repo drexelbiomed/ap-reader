d0db89ed8b91605fce0a29901e8ea69981920968
Carlos Diaz <cdiaz86@gmail.com>
2020-07-23 17:07:46 -0400

    Captures AP credit based off passing scores.

--- assignCourseCredit.js
+++ assignCourseCredit.js
@@ -2,17 +2,29 @@ const studentApData = require('./extractApData.js');
 const apCrosswalkData = require('./extractCrosswalkData.js');
 
 function apCreditAssignment(students, apTests) {
+  // TODO capture final information in array of credits awarded.
+  const creditsAwarded = [];
   for (let student of Object.values(students)) {
-    if (student.id === 14347618) {
+    // if (student.id === 14347618) {
+      console.log(`******* ${student.name} advised by ${student.advisor}`);
       for (let testResult of student.tests) {
         for (let apTest of apTests) {
-          if (apTest.number === testResult.testcode) {
-            console.log(`${student.name} took the ${apTest.number}: ${apTest.name}`);
+          
+          if (testResult.testcode === apTest.number) {
+            // TODO Before returning values, make sure credit has not already been awarded for AP test.
+            if (Number(testResult.testscore) === 5 || Number(testResult.testscore) === 4) {
+              console.log("***");
+              console.log(`*** Score Required for ${apTest.name}: ${apTest.score}, Score Earned: ${testResult.testscore}`);
+              console.log(`*** Earned credit for ${apTest.courseAwarded}: ${apTest.creditAwarded} credits`);
+              console.log("***");
+            }
           }
         }
       }
-    }
+    // }
   }
+
+  return creditsAwarded;
 }
 
 apCreditAssignment(studentApData, apCrosswalkData)
