54c2ecb4bab70fc03742dd8c5728059c7cfd8f36
Carlos Diaz <cdiaz86@gmail.com>
2020-07-24 12:15:55 -0400

    Captures credit awarded.

--- assignCourseCredit.js
+++ assignCourseCredit.js
@@ -1,30 +1,30 @@
 const studentApData = require('./extractApData.js');
 const apCrosswalkData = require('./extractCrosswalkData.js');
-
+// TODO Scores of 4 are awarding AP Calc credit for both MATH 121 and MATH 122. Fix this.
 function apCreditAssignment(students, apTests) {
-  // TODO capture final information in array of credits awarded.
-  const creditsAwarded = [];
   for (let student of Object.values(students)) {
-      console.log('********************************************************');
-      console.log(`${student.name} advised by ${student.advisor}`);
-      for (let testResult of student.tests) {
-        for (let apTest of apTests) {
-          
-          if (testResult.testcode === apTest.number) {
-            // TODO Before returning values, make sure credit has not already been awarded for AP test.
-            if (Number(testResult.testscore) === 5 || Number(testResult.testscore) === 4) {
-              console.log("*********");
-              console.log(`Score Required for ${apTest.name}: ${apTest.score}, Score Earned: ${testResult.testscore}`);
-              console.log(`Earned credit for ${apTest.courseAwarded}: ${apTest.creditAwarded} credits`);
-              console.log("*********");
+    student.creditAwarded = [];
+    for (let testResult of student.tests) {
+      for (let apTest of apTests) {
+        
+        if (testResult.testcode === apTest.number) {
+          // TODO Before returning values, make sure credit has not already been awarded for AP test.
+          if (Number(testResult.testscore) === 5 || Number(testResult.testscore) === 4) {
+            const courseEarned = {
+              testName: apTest.name,
+              testScoreRequired: apTest.score,
+              scoreEarned: testResult.testscore,
+              courseAwarded: apTest.courseAwarded,
+              creditsAwarded: apTest.creditAwarded
             }
+            student.creditAwarded.push(courseEarned);
           }
         }
       }
-    console.log('********************************************************');
+    }
   }
-
-  return creditsAwarded;
+  console.log(students['14347618']);
+  return students
 }
 
 apCreditAssignment(studentApData, apCrosswalkData)
